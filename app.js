62416dda36b7a5885a92e0f62e154c691e82f6e1
SproutStudent <sproutstudent@Mai-D.local>
2020-06-30 11:07:55 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,28 @@
+
+
+function myFunction(img) {
+    // // Get the expanded image
+    //  var expandImg = document.getElementById("expandedImg");
+    // // Use the same src in the expanded image as the image being clicked on from the grid
+    //  expandImg.src = imgs.src;
+    // Show the container element (hidden with CSS)
+    //  expandImg.parentElement.style.display = "block";
+        // Get the expanded image
+        var expandImg = document.querySelectorAll(".rental-property");
+        // Use the same src in the expanded image as the image being clicked on from the grid
+         expandImg.src = img.src;
+        // Show the container element (hidden with CSS)
+        // expandImg.parentElement.style.display = "block";
+  }
+ 
+ 
+
+
+  $('.currency-container').slideToggle();
+  $(".dollar").click(function(){
+      $(".currency-container").slideToggle();
+    });
+  
+  
+
+  
